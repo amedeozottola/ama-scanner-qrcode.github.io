11850b300304bad82ae53f6f24233ae1afcba588
root <amedeo.zottola@grupposandonato.it>
2026-04-17 15:21:42 +0200

    aggiunta icone pubblica per generazione QR

--- sw.js
+++ sw.js
@@ -6,6 +6,7 @@ self.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then((cache) => {
       // Inserisci qui solo i file INDISPENSABILI (es. la tua index e il css principale)
       return cache.addAll([
+        './index.html',
         './scansiona-ticket.html'
       ]);
     })
